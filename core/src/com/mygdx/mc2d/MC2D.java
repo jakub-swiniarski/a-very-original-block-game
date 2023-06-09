e9ead009f354d3053d91b9a11967fb0a002b57db
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-06-09 17:47:55 +0200

    fullscreen on/off

--- core/src/com/mygdx/mc2d/MC2D.java
+++ core/src/com/mygdx/mc2d/MC2D.java
@@ -1,6 +1,8 @@
 package com.mygdx.mc2d;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,16 +10,27 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class MC2D extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	boolean fullscreen;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		fullscreen=false;
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
+		if(Gdx.input.isKeyJustPressed(Input.Keys.F11)){
+			fullscreen=!fullscreen;
+		}
+		if(fullscreen){
+			Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
+		}
+		else{
+			Gdx.graphics.setWindowedMode(1600,900);
+		}
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
--- desktop/src/com/mygdx/mc2d/DesktopLauncher.java
+++ desktop/src/com/mygdx/mc2d/DesktopLauncher.java
@@ -4,11 +4,11 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.mc2d.MC2D;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
+		config.setWindowedMode(1600,900);
+		config.useVsync(true);
 		config.setTitle("Minecraft 2D");
 		new Lwjgl3Application(new MC2D(), config);
 	}
