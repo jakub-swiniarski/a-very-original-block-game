7ad8a1f0b5d40578d581b5da3663906cd90ebe45
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-06-09 19:10:34 +0200

    removed redundant variables and imports

--- core/src/com/mygdx/mc2d/Block.java
+++ core/src/com/mygdx/mc2d/Block.java
@@ -1,14 +1,11 @@
 package com.mygdx.mc2d;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Block {
     Rectangle rect;
     Texture img;
-    int id;
 
     public Block(){
         rect=new Rectangle();
